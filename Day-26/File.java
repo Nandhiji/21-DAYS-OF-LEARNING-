563fd1d36fbf15c4f3f7e4535323f5ac54ef9573
Nandhini390-creator <sakthihariharan390@gmail.com>
2026-03-14 11:28:55 +0530

    exception handling

---
+++ Day-26/File.java
@@ -0,0 +1,54 @@
+bank withdrawal example using try-catch-finally block in Java
+class Fi{
+    int balance=3000;
+
+    void withdraw(int amount){
+        if(amount>balance){
+            throw new ArithmeticException("Insufficient balance");
+        }
+        balance-=amount;
+        System.out.println("Withdrawal successful. Remaining balance: "+balance);
+
+    }
+}
+public class File{
+
+    public static void main(String[] args) {
+        Fi di=new Fi();
+
+        try {
+            di.withdraw(100);
+        } catch (Exception e) {
+            System.out.println("error: "+e.getMessage());
+        }
+        finally{
+            System.out.println("transaction completed");
+        }
+    }
+}
+login system example using try-catch-finally block in Java
+
+class Fi{
+    void password(String password){
+        if(!password.equals("admin893")){
+            throw new ArithmeticException("wrong password");
+        }
+        else{
+            System.out.println("password is correct");
+        }
+    }
+}
+public class File{
+    public static void main(String[] args) {
+        Fi nu =new Fi();
+
+        try {
+            nu.password("admin893");
+        } catch (Exception e) {
+            System.out.println("error: "+e.getMessage());
+        }
+        finally{
+            System.out.println("process completed");
+        }
+    }
+}
